5a5dfc53e8e9273f11eaaaf825bae86edf3ba488
totti abiansyah <103946109+tottiabi@users.noreply.github.com>
2023-12-26 00:13:45 +0700

    Update TB_FUNGSIO.py

--- TB_FUNGSIO.py
+++ TB_FUNGSIO.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 # Pure function untuk menghitung total pembayaran
 def calculate_total(items):
-    return sum(item[1] * item[2] for item in items)
+    return sum(float(item[1]) * float(item[2]) for item in items)
 
 # Closure untuk menyimpan riwayat transaksi
 def transaction_history():
@@ -13,7 +13,8 @@ def transaction_history():
         history.append({'items': items, 'total': total})
         print("Riwayat Transaksi:")
         for idx, transaction in enumerate(history, start=1):
-            print(f"Transaksi ke-{idx}: {transaction['items']} - Total: {transaction['total']}")
+            print(
+                f"Transaksi ke-{idx}: {transaction['items']} - Total: {transaction['total']}")
 
     return add_to_history
 
@@ -51,7 +52,8 @@ def create_gui():
         total_label.config(text=f"Total Pembayaran: Rp {total:,}")
 
     def add_to_cart():
-        item = tuple(map(lambda x: x.get(), (item_name, item_price, item_quantity)))
+        item = tuple(
+            map(lambda x: x.get(), (item_name, item_price, item_quantity)))
         cart.append(item)
         item_name.delete(0, tk.END)
         item_price.delete(0, tk.END)
@@ -81,7 +83,8 @@ def create_gui():
     item_quantity = tk.Entry(root)
     item_quantity.pack()
 
-    add_to_cart_button = tk.Button(root, text="Tambah ke Keranjang", command=add_to_cart)
+    add_to_cart_button = tk.Button(
+        root, text="Tambah ke Keranjang", command=add_to_cart)
     add_to_cart_button.pack()
 
     calculate_button = tk.Button(root, text="Hitung Total", command=calculate)
@@ -90,10 +93,12 @@ def create_gui():
     total_label = tk.Label(root, text="Total Pembayaran: Rp 0")
     total_label.pack()
 
-    history_button = tk.Button(root, text="Tampilkan Riwayat Transaksi", command=show_history)
+    history_button = tk.Button(
+        root, text="Tampilkan Riwayat Transaksi", command=show_history)
     history_button.pack()
 
     root.mainloop()
+
 
 # Menjalankan GUI
 if __name__ == "__main__":
